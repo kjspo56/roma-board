2dcab3c03ce96f4b5e0ae5a040fb62d60ab5b05b
tedkim <kjspo56@gmail.com>
2023-05-03 18:15:09 +0900

    [F] test vue

--- frontend/roma-front/src/App.vue
+++ frontend/roma-front/src/App.vue
@@ -1,10 +1,21 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <PageHeader/> <!--헤더 컴포넌트 -->
+  <router-view/> <!--페이지 이동이 표시될 곳 -->
+  <PageFooter/> <!-- 푸터 컴포넌트 -->
 </template>
+
+<script>
+import PageHeader from '@/components/PageHeader'
+import PageFooter from '@/components/PageFooter'
+
+export default {
+  name : 'App',
+  components: {
+    PageFooter,
+    PageHeader
+  }
+}
+</script>
 
 <style>
 #app {
