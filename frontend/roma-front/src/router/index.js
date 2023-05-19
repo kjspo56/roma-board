5aa36c5ecaeb89895dbf713d6177741046d16fc8
Roma_Man <kjspo56@gmail.com>
2023-05-19 12:06:28 +0900

    [F] 테스트 다시 원복

--- frontend/roma-front/src/router/index.js
+++ frontend/roma-front/src/router/index.js
@@ -2,10 +2,6 @@ import { createRouter, createWebHistory } from 'vue-router'
 //import HomeView from '../views/HomeView.vue'
 import PageHome from "@/views/PageHome";
 
-import PostCreateView from "@/views/post/PostCreateView.vue";
-import PostListView from "@/views/post/PostListView.vue";
-import PostDetailView from "@/views/post/PostDetailView.vue";
-import PostEditView from "@/views/post/PostEditView.vue";
 
 const routes = [
   {
@@ -17,26 +13,6 @@ const routes = [
     path: '/about',
     name: 'About',
     component: () => import(/* webpackChunkName: "about" */ '../views/PageAbout.vue')
-  },
-  {
-    path: '/post',
-    name: 'PostList',
-    component: PostListView
-  },
-  {
-    path: '/post',
-    name: 'PostCreate',
-    component: PostCreateView
-  },
-  {
-    path: '/post',
-    name: 'PostDetail',
-    component: PostDetailView
-  },
-  {
-    path: '/post',
-    name: 'PostEdit',
-    component: PostEditView
   }
 ]
 
