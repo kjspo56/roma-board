057149cd14d8f9ef31de66b182556caa9073f5a1
kjspo56 <kjspo56@gmail.com>
2023-11-22 23:34:26 +0900

    [F] formatDate 수정

--- frontend/roma-front/src/components/PageMain.vue
+++ frontend/roma-front/src/components/PageMain.vue
@@ -7,7 +7,6 @@
       <tr>
         <th scope="col">ID</th>
         <th scope="col">Title</th>
-        <th scope="col">Contents</th>
         <th scope="col">Writer</th>
         <th scope="col">Like</th>
         <th scope="col">Date</th>
@@ -15,24 +14,51 @@
       </tr>
       </thead>
       <tbody>
-      <tr>
-        <td>1</td>
-        <td>제목 입니다</td>
-        <td>내용 입니다</td>
-        <td>작성자 입니다</td>
-        <td>좋아요 수</td>
-        <td>날짜</td>
-        <td>조회수</td>
+      <tr v-for="post in result" v-bind:key="post.postId">
+        <td>{{ post.postId }}</td>
+        <td>{{ post.title }}</td>
+        <td>{{ post.writer }}</td>
+        <td>{{ post.postLike }}</td>
+        <td>{{ formatDateTime(post.regDate) }}</td>
+        <td>{{ post.view }}</td>
       </tr>
       </tbody>
     </table>
   </div>
+  <div class="d-flex">
+    <button type="button" class="btn btn-primary" >글쓰기</button>
+  </div>
 </template>
 
 <script>
+import axios from "axios";
+import moment from "moment";
 export default {
   name: "PageMain",
-}
+  data() {
+    return {
+      result: []
+    };
+  },
+  created() {
+    this.getListData();
+  },
+  methods: {
+    getListData() {
+      axios.post('http://localhost:9096/post/list')
+          .then(res => {
+            console.log(res)
+            this.result = res.data.data;
+          })
+          .catch((error) => {
+            console.log("Error fetching postList:", error);
+          })
+    },
+    formatDateTime(dateTime) {
+      return moment(dateTime).format("YYYY-MM-DD HH:mm");
+    }
+  },
+};
 </script>
 
 <style scoped>
